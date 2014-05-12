f3873d18b0cdcfe2776f351308dd0826e75e7537
Linker Lin <linker.lin@live.com>
2014-05-12 22:56:50 +0800

    added original SimpleInputProvider

--- nn-samples/src/test/java/com/github/neuralnetworks/test/SimpleInputProvider.java
+++ nn-samples/src/test/java/com/github/neuralnetworks/test/SimpleInputProvider.java
@@ -1,119 +1,37 @@
 package com.github.neuralnetworks.test;
 
-import com.github.neuralnetworks.calculation.neuronfunctions.TensorFunction;
-import com.github.neuralnetworks.input.InputConverter;
-import com.github.neuralnetworks.training.TrainingInputData;
-import com.github.neuralnetworks.training.TrainingInputProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import com.github.neuralnetworks.training.TrainingInputProviderImpl;
 
 /**
- * Created by linkerlin on 5/12/14.
+ * Simple input provider for testing purposes.
+ * Training and target data are two dimensional float arrays
  */
-public class SimpleInputProvider implements TrainingInputProvider {
-    float[][] X;
-    float[][] Y;
-    private Random random=new Random();
-    /**
-     * List of modifiers to apply on the input data after the conversion
-     */
-    private List<TensorFunction> inputModifiers;
-    /**
-     * Converter for the target
-     */
-    private InputConverter inputConverter;
-    public SimpleInputProvider(){
-        this(null);
-    }
-    public SimpleInputProvider(InputConverter inputConverter){
+public class SimpleInputProvider extends TrainingInputProviderImpl {
+
+    private static final long serialVersionUID = 1L;
+
+    private float[][] input;
+    private float[][] target;
+
+    public SimpleInputProvider(float[][] input, float[][] target) {
         super();
-        this.inputConverter = inputConverter;
+
+        this.input  = input;
+        this.target = target;
     }
-    public SimpleInputProvider(float[][] X, float[][] Y){
-        this(X,Y,null);
-    }
-    public SimpleInputProvider(float[][] X, float[][] Y,InputConverter inputConverter){
-        super();
-        this.X=X.clone();
-        this.Y=Y.clone();
-        this.inputConverter = inputConverter;
-    }
+
     @Override
     public int getInputSize() {
-        return X.length;
-    }
-    /**
-     * Counter
-     */
-    protected int currentInput=0;
-    protected int currentTarget=0;
-    @Override
-    public void reset() {
-        this.currentInput=0;
-        this.currentTarget=0;
+        return input.length;
     }
 
     @Override
     public float[] getNextInput() {
-        return this.X[currentInput++];
+        return input[currentInput % input.length];
     }
 
     @Override
     public float[] getNextTarget() {
-        return this.Y[currentTarget++];
+        return target[currentInput % target.length];
     }
-
-    @Override
-    public List<TensorFunction> getInputModifiers() {
-        return inputModifiers;
-    }
-
-    public void addInputModifier(TensorFunction modifier) {
-        if (inputModifiers == null) {
-            inputModifiers = new ArrayList<>();
-        }
-
-        inputModifiers.add(modifier);
-    }
-
-    public void removeModifier(TensorFunction modifier) {
-        if (inputModifiers != null) {
-            inputModifiers.remove(modifier);
-        }
-    }
-
-    @Override
-    public void afterBatch(TrainingInputData ti) {
-        reset();
-    }
-
-    @Override
-    public void beforeBatch(TrainingInputData ti) {
-
-    }
-
-    @Override
-    public void afterSample() {
-        this.currentInput++;
-        this.currentTarget++;
-    }
-
-    @Override
-    public void beforeSample() {
-        currentInput=random.nextInt(getInputSize());
-        currentTarget=currentInput;
-    }
-    public InputConverter getInputConverter() {
-        return inputConverter;
-    }
-    public void setInputConverter(InputConverter inputConverter) {
-        this.inputConverter = inputConverter;
-    }
-
-//    @Override
-//    public void populateNext(TrainingInputData ti) {
-//
-//    }
 }
